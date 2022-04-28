4366b4009ceba01925d072df776e2a27cd6ca651
Joni Savolainen <joni@savolainen.io>
2022-04-28 19:17:46 +0300

    feat: add a section for UnityGit to project settings

---
+++ Core/Components/SettingsProviderView.cs
@@ -0,0 +1,52 @@
+﻿using UIComponents.Core;
+using UnityEngine.UIElements;
+using UnityGit.Core;
+
+namespace UnityGit.GUI.Components
+{
+    [Layout("SettingsProviderView/SettingsProviderView.uxml")]
+    [Stylesheet("SettingsProviderView/SettingsProviderView.style.uss")]
+    [InjectDependency(typeof(IUnityGitStatus), provider: typeof(UnityGitStatus))]
+    public class SettingsProviderView : UIComponent
+    {
+        private readonly IUnityGitStatus _unityGitStatus;
+
+        private readonly VisualElement _infoContainer;
+        
+        public SettingsProviderView()
+        {
+            _unityGitStatus = Provide<UnityGitStatus>();
+            
+            var refreshRepositoriesButton = this.Q<Button>("settings-provider-refresh-repositories-button");
+
+            refreshRepositoriesButton.clicked += RefreshRepositories;
+            
+            AddToClassList("container");
+
+            _infoContainer = this.Q<VisualElement>("settings-provider-info-container");
+            
+            RefreshInfo();
+        }
+
+        private void RefreshRepositories()
+        {
+            _unityGitStatus.PopulateRepositories();
+            RefreshInfo();
+        }
+
+        private void RefreshInfo()
+        {
+            _infoContainer.Clear();
+            
+            if (_unityGitStatus.HasProjectRepository())
+                _infoContainer.Add(new Label("Project repository found."));
+            else
+                _infoContainer.Add(new Label("Project repository not found."));
+
+            if (_unityGitStatus.HasPackageRepositories())
+                _infoContainer.Add(new Label($"{_unityGitStatus.PackageRepositories.Count} package repositories found."));
+            else
+                _infoContainer.Add(new Label("No package repositories found."));
+        }
+    }
+}
---
+++ Core/Settings/UnityGitSettingsProviderRegister.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityGit.GUI.Components;
+
+namespace UnityGit.GUI.Settings
+{
+    public static class UnityGitSettingsProviderRegister
+    {
+        [SettingsProvider]
+        public static SettingsProvider CreateUnityGitSettingsProvider()
+        {
+            var provider = new SettingsProvider("Project/UnityGit", SettingsScope.Project)
+            {
+                label = "UnityGit",
+                activateHandler = (searchContext, rootElement) =>
+                {
+                    rootElement.Add(new SettingsProviderView());
+                },
+                keywords = new HashSet<string>(new[] {"Git", "Repository"})
+            };
+
+            return provider;
+        }
+    }
+}
