6fa908e503383340fb39d562816073220cea16f5
Joni Savolainen <joni@savolainen.io>
2022-06-04 13:09:28 +0300

    chore: create Service class

---
+++ Assets/UnityGit/Core/Services/Service.cs
@@ -0,0 +1,19 @@
+﻿using UIComponents;
+
+namespace UnityGit.Core.Services
+{
+    public abstract class Service
+    {
+        private readonly DependencyInjector _dependencyInjector;
+
+        protected Service()
+        {
+            _dependencyInjector = DependencyInjector.GetInjector(GetType());
+        }
+        
+        protected T Provide<T>() where T : class
+        {
+            return _dependencyInjector.Provide<T>();
+        }
+    }
+}
